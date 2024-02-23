69f11b46d5a719964801bb02c4796282ac2019c5
Denis <39241104+DenisTonyshev@users.noreply.github.com>
2024-02-24 00:15:56 +0200

    in progress NavBar

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+export function RenderHeader() { 
+    return (
+    <header className="App-header">
+    <p>
+      HELLO WORLD
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+
+    );
+
+}
